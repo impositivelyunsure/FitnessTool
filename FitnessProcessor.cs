e82178257ac73f6103142721793bb7169174c582
positivelyunsure <positivelyunsure@proton.me>
2026-03-19 20:37:15 +0800

    created the API & macro method in processor

--- FitnessProcessor.cs
+++ FitnessProcessor.cs
@@ -1,11 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using static FitnessLibrary.FitnessProcessorLib;
+
 
 namespace FitnessTool
 {
     class FitnessProcessor
     {
+        public void AddMacros(string proteinsInput, string fatsInput, string carbsInput)
+        {
+            if (Double.TryParse(proteinsInput, out double resultProteins))
+            {
+                if (Double.TryParse(fatsInput, out double resultFats))
+                {
+                    if (Double.TryParse(carbsInput, out double resultCarbs))
+                    {
+                        var APIobject = new FitnessAPIClient();
+                    }
+
+                }
+            }
+        }
+
+
+
 
     }
 }
